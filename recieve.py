a41362fc8dc43b02f77580b409d6d375cc13710e
Adam Novotny <adam.novotny@postrock.wall>
2017-04-20 20:21:55 -0400

    Init commit

---
+++ recieve.py
@@ -0,0 +1,34 @@
+import pyaudio
+import wave
+import numpy as np
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 48000
+CHUNK = 1024 # Buffer size
+
+audio = pyaudio.PyAudio()
+
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK)*2):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+
+# Stop the stream
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+
+
+waveFile = wave.open("rec.wav", 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
