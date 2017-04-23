e60c4f03694374b69dd770a92d9344710a992142
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2017-04-22 23:05:42 -0400

    Made sending automatic

--- send.py
+++ send.py
@@ -1,8 +1,15 @@
 from signal_functions import *
 import time
+import sys
 
-# data = [1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0]
-data = [1, 0, 0, 1]
+character_bin = bin(ord(sys.argv[1][0]))[2:]
+character_bin_pad = [0 for i in range(8-len(character_bin))] + [[0,1][int(a)] for a in character_bin]
+
+print(character_bin_pad)
+
+data = [1, 0, 1, 0] + character_bin_pad + [1, 0]
+
+# data = [1, 0, 0, 1]
 print("Making signal. Hz:", CARRIER_FREQ, "\tClock:", str(CLOCK_MS) + "ms", \
         "\tSampling Rate:", SAMPLING_RATE)
 
