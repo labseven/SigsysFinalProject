af82f225a8dd3cd03331ae51862174111e9db1f7
Adam Novotny <adam.novotny@postrock.wall>
2017-04-27 15:08:14 -0400

    Demo Day 2

---
+++ filter_method.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import scipy.signal
+import numpy as np
+import time
+from signal_functions import *
+
+def filter_signal():
+    match_filter = make_match_filter()
+    signal_in = import_wav("rec.wav")
+
+
+    # plot_waveform(match_filter, downsample=1, title="Match Filter", ax_labels=["Samples", "Magnitude"])
+    # plot_signal(signal_in, downsample=1)
+
+    envelope, convolution = get_envelope(signal_in[:600000])
+
+
+    # plot_waveform(convolution[:350000], downsample=10, title="5kHz Signal after Convolution", ax_labels=["Samples", "Magnitude"])
+    # plot_waveform(envelope[:35000], downsample=1, title="5kHz Signal after Convolution", ax_labels=["Samples", "Magnitude"])
+    # plot_signal(convolution)
+
+
+    interrupt_t, thresholds = find_intterupts(envelope)
+    data, packet = extract_data(interrupt_t)
+
+
+    plot_envelope_interrupts(envelope, interrupt_t, thresholds)
+
+
+    print("Packet:", packet, "Bits:", len(packet) + 1)
+    print("Data:", chr(data))
